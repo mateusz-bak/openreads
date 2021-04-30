bf176150a61cb9ab170585dde3252ea878492c74
mateusz-bak <mateusz.bak@protonmail.com>
2021-04-30 12:40:17 +0100

    Modified ListActivity.kt to be a fragment holder.

--- app/src/main/java/com/example/mybooks/ui/bookslist/ListActivity.kt
+++ app/src/main/java/com/example/mybooks/ui/bookslist/ListActivity.kt
@@ -2,22 +2,20 @@ package com.example.mybooks.ui.bookslist
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.mybooks.other.ReadBookAdapter
 import com.example.mybooks.R
-import com.example.mybooks.data.db.ReadBooksDatabase
-import com.example.mybooks.data.repositories.ReadBooksRepository
+import com.example.mybooks.data.db.BooksDatabase
+import com.example.mybooks.data.repositories.BooksRepository
 import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
+
+    lateinit var booksViewModel: BooksViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,40 +23,22 @@ class ListActivity : AppCompatActivity() {
 
         bottomNavigationView.setupWithNavController(booksNavHostFragment.findNavController())
 
-//        val database = BooksDatabase(this)
-//        val repository = BooksRepository(database)
-//        val factory = BooksViewModelFactory(repository)
-//
-//        val viewModel = ViewModelProviders.of(this, factory).get(BooksViewModel::class.java)
-//
-//        val adapter = ListElementAdapter(listOf(), viewModel, this)
-//
-//        rvListOfBooks.adapter = adapter
-//        rvListOfBooks.layoutManager = LinearLayoutManager(this)
-//
-//        viewModel.getAllListElements().observe(this, Observer {
-//            adapter.books = it
-//            adapter.notifyDataSetChanged()
-//        })
-//
-//        fabGoToAdder.setOnClickListener {
-//            Intent(this, AdderActivity::class.java).also {
-//                startActivity(it)
-//            }
-//        }
-//    }
-//
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.app_bar_menu, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when(item.itemId) {
-//            R.id.miAbout -> Intent(this, AboutActivity::class.java).also {
-//                startActivity(it)
-//            }
-//        }
-//        return true
+        val booksRepository = BooksRepository(BooksDatabase(this))
+        val booksViewModelProviderFactory = BooksViewModelProviderFactory(booksRepository)
+        booksViewModel = ViewModelProvider(this, booksViewModelProviderFactory).get(
+            BooksViewModel::class.java)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miAbout -> Intent(this, AboutActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        return true
     }
 }
