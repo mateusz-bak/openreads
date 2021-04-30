9daa5522c2cca065aac889caea61a6fd6e78a498
mateusz-bak <mateusz.bak@protonmail.com>
2021-04-30 12:39:34 +0100

    Removed redundant files.

--- app/src/main/java/com/example/mybooks/ui/bookslist/EditReadBookDialogListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mybooks.ui.bookslist
-
-import com.example.mybooks.data.db.entities.ReadBook
-
-interface EditReadBookDialogListener {
-    fun onSaveButtonClicked(item: ReadBook)
-}
