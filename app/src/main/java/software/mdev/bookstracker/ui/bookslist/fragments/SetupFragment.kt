e7e9cf5a9b356a6e3c6d1e2cf7347775b7ded76c
mateusz-bak <mateusz.bak@protonmail.com>
2021-06-08 23:32:37 +0100

    Setup fragment will show up only once if button is clicked.

--- app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/SetupFragment.kt
+++ app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/SetupFragment.kt
@@ -1,13 +1,12 @@
 package software.mdev.bookstracker.ui.bookslist.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import software.mdev.bookstracker.R
-import software.mdev.bookstracker.data.db.entities.Book
-import software.mdev.bookstracker.ui.bookslist.viewmodel.BooksViewModel
 import software.mdev.bookstracker.ui.bookslist.ListActivity
 import kotlinx.android.synthetic.main.fragment_setup.*
 
@@ -17,8 +16,32 @@ class SetupFragment : Fragment(R.layout.fragment_setup) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val sharedPref = (activity as ListActivity).getSharedPreferences("appPref", Context.MODE_PRIVATE)
+
+        if(sharedPref.getBoolean("app_launched", false)) {
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.setupFragment, true)
+                .build()
+            findNavController().navigate(
+                R.id.action_setupFragment_to_readFragment,
+                savedInstanceState,
+                navOptions
+            )
+        }
+
         fabLaunchApp.setOnClickListener{
+            saveAppsFirstlaunch()
             findNavController().navigate(R.id.action_setupFragment_to_readFragment)
+        }
+    }
+
+    private fun saveAppsFirstlaunch() {
+        val sharedPref = (activity as ListActivity).getSharedPreferences("appPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        editor.apply {
+            putBoolean("app_launched", true)
+            apply()
         }
     }
 }
