cd07784dd90ce51582d2977c310e19486f756fc4
mateusz-bak <mateusz.bak@protonmail.com>
2021-06-14 23:27:59 +0100

    Added update check when opening the app and in the settings.

--- app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/SettingsFragment.kt
+++ app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/SettingsFragment.kt
@@ -1,15 +1,25 @@
 package software.mdev.bookstracker.ui.bookslist.fragments
 
 import android.app.Activity
+import android.content.Context
 import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
+import com.github.javiersantos.appupdater.AppUpdater
+import com.github.javiersantos.appupdater.enums.Display
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import software.mdev.bookstracker.R
+import software.mdev.bookstracker.other.Constants
+import software.mdev.bookstracker.other.Constants.KEY_CHECK_FOR_UPDATES
 import software.mdev.bookstracker.other.Constants.SHARED_PREFERENCES_KEY_ACCENT
 import software.mdev.bookstracker.other.Constants.SHARED_PREFERENCES_KEY_RECOMMENDATIONS
 import software.mdev.bookstracker.other.Constants.SHARED_PREFERENCES_REFRESHED
@@ -24,6 +34,15 @@ class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeLis
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         viewModel = (activity as ListActivity).booksViewModel
+
+        var preferenceCheckForUpdates = findPreference<Preference>(KEY_CHECK_FOR_UPDATES)
+
+        if (preferenceCheckForUpdates != null) {
+            preferenceCheckForUpdates.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                context?.let { it1 -> checkForAppUpdate(it1) }
+                true
+            }
+        }
     }
 
     override fun onResume() {
@@ -56,5 +75,22 @@ class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeLis
         sharedPref.edit().putBoolean(SHARED_PREFERENCES_REFRESHED, true).apply()
         findNavController().navigate(R.id.readFragment, null)
         (activity as FragmentActivity).recreate()
+    }
+
+    fun checkForAppUpdate(context: Context) = CoroutineScope(Dispatchers.Main).launch {
+        var appUpdater = AppUpdater(context)
+        appUpdater
+            .setTitleOnUpdateAvailable(getString(R.string.setTitleOnUpdateAvailable))
+            .setContentOnUpdateAvailable(getString(R.string.setContentOnUpdateAvailable))
+            .setButtonUpdate(getString(R.string.setButtonUpdate))
+            .setButtonDismiss(getString(R.string.setButtonDismiss))
+            .setButtonDoNotShowAgain(getString(R.string.setButtonDoNotShowAgain))
+            .setContentOnUpdateNotAvailable(getString(R.string.setContentOnUpdateNotAvailable))
+            .setTitleOnUpdateNotAvailable(getString(R.string.setTitleOnUpdateNotAvailable))
+            .setUpdateFrom(UpdateFrom.GITHUB)
+            .setGitHubUserAndRepo(Constants.GITHUB_USER, Constants.GITHUB_REPO)
+            .setDisplay(Display.DIALOG)
+            .showAppUpdated(true)
+            .start()
     }
 }
