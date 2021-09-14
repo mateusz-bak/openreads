16293fdadba4e34b0aa1f69b0c1aca380bac30ec
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2021-09-14 23:08:56 +0100

    Added warnings when deleting a book.

--- app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/DisplayBookFragment.kt
+++ app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/DisplayBookFragment.kt
@@ -25,6 +25,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 import android.graphics.*
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
@@ -294,30 +295,73 @@ class DisplayBookFragment : Fragment(R.layout.fragment_display_book) {
         }
 
         fabDeleteBook.setOnClickListener{
-            viewModel.updateBook(
-                book.id,
-                book.bookTitle,
-                book.bookAuthor,
-                book.bookRating,
-                book.bookStatus,
-                book.bookPriority,
-                book.bookStartDate,
-                book.bookFinishDate,
-                book.bookNumberOfPages,
-                book.bookTitle_ASCII,
-                book.bookAuthor_ASCII,
-                true,
-                book.bookCoverUrl,
-                book.bookOLID,
-                book.bookISBN10,
-                book.bookISBN13
-            )
-            recalculateChallenges(book.bookStatus)
+            val deleteBookWarningDialog = this.context?.let { it1 ->
+                AlertDialog.Builder(it1)
+                    .setTitle(R.string.warning_delete_book_title)
+                    .setMessage(R.string.warning_delete_book_message)
+                    .setIcon(R.drawable.ic_baseline_warning_amber_24)
+                    .setNegativeButton(R.string.warning_delete_book_delete) { _, _ ->
+                        viewModel.updateBook(
+                            book.id,
+                            book.bookTitle,
+                            book.bookAuthor,
+                            book.bookRating,
+                            book.bookStatus,
+                            book.bookPriority,
+                            book.bookStartDate,
+                            book.bookFinishDate,
+                            book.bookNumberOfPages,
+                            book.bookTitle_ASCII,
+                            book.bookAuthor_ASCII,
+                            true,
+                            book.bookCoverUrl,
+                            book.bookOLID,
+                            book.bookISBN10,
+                            book.bookISBN13
+                        )
+                        recalculateChallenges(book.bookStatus)
 
-            Snackbar.make(it, getString(R.string.bookDeleted), Snackbar.LENGTH_LONG)
-                .setAction(getString(R.string.undo), UndoBookDeletion())
-                .show()
+                        Snackbar.make(it, getString(R.string.bookDeleted), Snackbar.LENGTH_LONG)
+                            .setAction(getString(R.string.undo), UndoBookDeletion())
+                            .show()
+                    }
+                    .setPositiveButton(R.string.warning_delete_book_cancel) { _, _ ->
+                    }
+                    .create()
+            }
+
+            deleteBookWarningDialog?.show()
+            if (this.context !=null && deleteBookWarningDialog?.getButton(AlertDialog.BUTTON_POSITIVE) != null) {
+                deleteBookWarningDialog?.getButton(AlertDialog.BUTTON_POSITIVE)!!.setBackgroundColor(getAccentColor(this.requireContext()))
+                deleteBookWarningDialog?.getButton(AlertDialog.BUTTON_POSITIVE)!!.setTextColor(ContextCompat.getColor(this.requireContext(),R.color.design_default_color_on_primary))
+            }
         }
+    }
+
+    private fun getAccentColor(context: Context): Int {
+
+        var accentColor = ContextCompat.getColor(context, R.color.purple_500)
+
+        val sharedPref = context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+        var accent = sharedPref?.getString(
+            Constants.SHARED_PREFERENCES_KEY_ACCENT,
+            Constants.THEME_ACCENT_DEFAULT
+        ).toString()
+
+        when(accent){
+            Constants.THEME_ACCENT_LIGHT_GREEN -> accentColor = ContextCompat.getColor(context, R.color.light_green)
+            Constants.THEME_ACCENT_ORANGE_500 -> accentColor = ContextCompat.getColor(context, R.color.orange_500)
+            Constants.THEME_ACCENT_CYAN_500 -> accentColor = ContextCompat.getColor(context, R.color.cyan_500)
+            Constants.THEME_ACCENT_GREEN_500 -> accentColor = ContextCompat.getColor(context, R.color.green_500)
+            Constants.THEME_ACCENT_BROWN_400 -> accentColor = ContextCompat.getColor(context, R.color.brown_400)
+            Constants.THEME_ACCENT_LIME_500 -> accentColor = ContextCompat.getColor(context, R.color.lime_500)
+            Constants.THEME_ACCENT_PINK_300 -> accentColor = ContextCompat.getColor(context, R.color.pink_300)
+            Constants.THEME_ACCENT_PURPLE_500 -> accentColor = ContextCompat.getColor(context, R.color.purple_500)
+            Constants.THEME_ACCENT_TEAL_500 -> accentColor = ContextCompat.getColor(context, R.color.teal_500)
+            Constants.THEME_ACCENT_YELLOW_500 -> accentColor = ContextCompat.getColor(context, R.color.yellow_500)
+        }
+        return accentColor
     }
 
     fun View.hideKeyboard() {
