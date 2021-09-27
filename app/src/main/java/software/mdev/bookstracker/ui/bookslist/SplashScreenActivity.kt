010e12feaaf0938ba00e238a577fc2ab343fc0e0
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2021-09-27 12:04:08 +0100

    Removed splash screen for debug build and decreased splash show time, changed fonts to sans-serif family.

--- app/src/main/java/software/mdev/bookstracker/ui/bookslist/SplashScreenActivity.kt
+++ app/src/main/java/software/mdev/bookstracker/ui/bookslist/SplashScreenActivity.kt
@@ -1,40 +1,16 @@
 package software.mdev.bookstracker.ui.bookslist
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_list.*
 import kotlinx.android.synthetic.main.activity_splash_screen.*
-import kotlinx.android.synthetic.main.item_setup.view.*
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import software.mdev.bookstracker.BuildConfig
 import software.mdev.bookstracker.R
-import software.mdev.bookstracker.data.db.BooksDatabase
-import software.mdev.bookstracker.data.db.LanguageDatabase
-import software.mdev.bookstracker.data.db.YearDatabase
-import software.mdev.bookstracker.data.repositories.BooksRepository
-import software.mdev.bookstracker.data.repositories.LanguageRepository
-import software.mdev.bookstracker.data.repositories.OpenLibraryRepository
-import software.mdev.bookstracker.data.repositories.YearRepository
-import software.mdev.bookstracker.other.Backup
-import software.mdev.bookstracker.other.Constants
-import software.mdev.bookstracker.other.Functions
-import software.mdev.bookstracker.other.Updater
 import software.mdev.bookstracker.ui.bookslist.viewmodel.BooksViewModel
-import software.mdev.bookstracker.ui.bookslist.viewmodel.BooksViewModelProviderFactory
-import java.io.IOException
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -44,11 +20,13 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        prepareSplashScreenViews()
-
-        animateSplashScreenViews()
-
-        launchListActivity()
+        if (BuildConfig.BUILD_TYPE == "debug")
+            launchListActivityNoDelay()
+        else {
+            prepareSplashScreenViews()
+            animateSplashScreenViews()
+            launchListActivityDelay()
+        }
     }
 
     private fun prepareSplashScreenViews() {
@@ -62,7 +40,7 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun animateSplashScreenViews() {
-        var duration = 600L
+        var duration = 400L
         var startDelay = 100L
 
         ivLogo.animate()
@@ -84,13 +62,19 @@ class SplashScreenActivity : AppCompatActivity() {
             .start()
     }
 
-    private fun launchListActivity() {
+    private fun launchListActivityDelay() {
         MainScope().launch {
-            delay(700)
+            delay(500L)
 
             val intent = Intent(this@SplashScreenActivity, ListActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun launchListActivityNoDelay() {
+        val intent = Intent(this@SplashScreenActivity, ListActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
