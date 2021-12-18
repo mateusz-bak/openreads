2db92b71de52a3b4960083e4806f4ec7a57f1ed4
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2021-12-18 23:49:03 +0000

    Updated app version and changelog.

--- app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/ChangelogFragment.kt
+++ app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/ChangelogFragment.kt
@@ -285,6 +285,16 @@ class ChangelogFragment : Fragment(R.layout.fragment_changelog) {
             getString(R.string.changelog_1_8_1_a)
         )
 
+        val version_1_9_0 = arrayOf(
+            getString(R.string.changelog_ver_1_9_0),
+            getString(R.string.changelog_date_1_9_0),
+            getString(R.string.changelog_1_9_0_a),
+            getString(R.string.changelog_1_9_0_b),
+            getString(R.string.changelog_1_9_0_c),
+            getString(R.string.changelog_1_9_0_d),
+            getString(R.string.changelog_1_9_0_e)
+        )
+
         return listOf(
             version_1_0_0,
             version_1_1_0,
@@ -299,7 +309,8 @@ class ChangelogFragment : Fragment(R.layout.fragment_changelog) {
             version_1_7_0,
             version_1_7_1,
             version_1_8_0,
-            version_1_8_1
+            version_1_8_1,
+            version_1_9_0
         )
     }
 
