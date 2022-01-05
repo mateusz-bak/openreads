b65e2667ebe217464ca51afc786136a8c922642b
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2022-01-05 18:54:35 +0000

    Backup is saved as json.

--- app/src/main/java/software/mdev/bookstracker/other/Backup.kt
+++ app/src/main/java/software/mdev/bookstracker/other/Backup.kt
@@ -6,14 +6,14 @@ import android.net.Uri
 import android.os.Handler
 import android.os.Looper
 import androidx.core.content.FileProvider.getUriForFile
-import androidx.sqlite.db.SimpleSQLiteQuery
 import software.mdev.bookstracker.BuildConfig
 import software.mdev.bookstracker.R
+import software.mdev.bookstracker.api.RetrofitInstance.Companion.gson
 import software.mdev.bookstracker.data.db.BooksDatabase
+import software.mdev.bookstracker.data.db.entities.Book
 import software.mdev.bookstracker.ui.bookslist.ListActivity
 import java.io.File
 import java.io.FileOutputStream
-import java.io.InputStreamReader
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -22,30 +22,31 @@ class Backup {
 
     fun exportAndShare(activity: ListActivity) {
         val thread = Thread {
-            val exported = exportBackup(activity)
+            val exported = exportBackupAsCsv(activity)
             if (exported.isNotBlank()) shareBackup(exported, activity)
         }
         thread.start()
     }
 
     // Export the room database to a file in Android/data/software.mdev.bookstracker(.debug)/files
-    private fun exportBackup(context: Context): String {
+    private fun exportBackupAsCsv(context: Context): String {
         // Perform a checkpoint to empty the write ahead logging temporary files and avoid closing the entire db
         val getBooksDao = BooksDatabase.getBooksDatabase(context).getBooksDao()
-        getBooksDao.checkpoint(SimpleSQLiteQuery("pragma wal_checkpoint(full)"))
 
-        val dbFile = context.getDatabasePath(Constants.DATABASE_FILE_NAME).absoluteFile
+        val books = getBooksDao.getBooksForBackup()
+        val booksJson = gson.toJson(books)
+
         val appDirectory = File(context.getExternalFilesDir(null)!!.absolutePath)
 
         val sdf = SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
         val currentDate = sdf.format(Date())
 
-        val fileName = "books_tracker_$currentDate.backup"
+        val fileName = "openreads_$currentDate.backup"
         val fileFullPath: String = appDirectory.path + File.separator.toString() + fileName
 
         // Snackbar need the ui thread to work, so they must be forced on that thread
         try {
-            dbFile.copyTo(File(fileFullPath), true)
+            File(fileFullPath).writeText(booksJson)
             (context as ListActivity).runOnUiThread {
                 (context as ListActivity).showSnackbar(context.getString(R.string.backup_success))
             }
@@ -79,63 +80,79 @@ class Backup {
     }
 
     // Import a backup overwriting any existing data and checking if the file is valid
-    fun importBackup(context: Context, fileUri: Uri): Boolean {
-        if (!isBackupValid(fileUri, context)) {
-            (context as ListActivity).showSnackbar(context.getString(R.string.backup_import_invalid_file))
-            return false
+    suspend fun importBackup(context: Context, fileUri: Uri): Boolean {
+        when (validateBackup(fileUri, context)) {
+            1 -> {
+                BooksDatabase.destroyInstance()
+
+                val fileStream = context.contentResolver.openInputStream(fileUri)!!
+                val dbFile = context.getDatabasePath(Constants.DATABASE_FILE_NAME).absoluteFile
+
+                try {
+                    fileStream.copyTo(FileOutputStream(dbFile))
+                    (context as ListActivity).showSnackbar(context.getString(R.string.backup_import_success))
+                } catch (e: Exception) {
+                    val string = context.getString(R.string.backup_import_failure) + " ERROR: ${e.toString()}"
+                    (context as ListActivity).showSnackbar(string)
+                    e.printStackTrace()
+                    return false
+                }
+                restartActivity(context)
+                return true
+            }
+            2 -> {
+                BooksDatabase.destroyInstance()
+
+                val fileStream = context.contentResolver.openInputStream(fileUri)!!
+                val json = fileStream.readBytes().toString(Charsets.UTF_8)
+
+                try {
+                    val booksBackup = gson.fromJson(json, Array<Book>::class.java).toList()
+
+                    val getBooksDao = BooksDatabase.getBooksDatabase(context).getBooksDao()
+                    val books = getBooksDao.getBooksForBackup()
+
+                    for (book in books)
+                            getBooksDao.delete(book)
+
+                    for (book in booksBackup)
+                            getBooksDao.upsert(book)
+
+                    (context as ListActivity).showSnackbar(context.getString(R.string.backup_import_success))
+                } catch (e: Exception) {
+                    val string = context.getString(R.string.backup_import_failure) + " ERROR: ${e.toString()}"
+                    (context as ListActivity).showSnackbar(string)
+                    e.printStackTrace()
+                    return false
+                }
+                restartActivity(context)
+                return true
+            }
+            else -> {
+                (context as ListActivity).showSnackbar(context.getString(R.string.backup_import_invalid_file))
+                return false
+            }
         }
+    }
 
-        BooksDatabase.destroyInstance()
-
-        val fileStream = context.contentResolver.openInputStream(fileUri)!!
-        val dbFile = context.getDatabasePath(Constants.DATABASE_FILE_NAME).absoluteFile
-
-        try {
-            fileStream.copyTo(FileOutputStream(dbFile))
-            (context as ListActivity).showSnackbar(context.getString(R.string.backup_import_success))
-        } catch (e: Exception) {
-            val string = context.getString(R.string.backup_import_failure) + " ERROR: ${e.toString()}"
-            (context as ListActivity).showSnackbar(string)
-            e.printStackTrace()
-            return false
-        }
+    private fun restartActivity(context: Context) {
         // Completely restart the application with a slight delay to be extra-safe
         val intent: Intent =
             (context as ListActivity).baseContext.packageManager.getLaunchIntentForPackage((context as ListActivity).baseContext.packageName)!!
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         Handler(Looper.getMainLooper()).postDelayed({ (context as ListActivity).startActivity(intent) }, 400)
-        return true
     }
 
     // Check if a backup file is valid. A wrong import would result in a crash or empty db
-    private fun isBackupValid(fileUri: Uri, context: Context): Boolean {
+    private fun validateBackup(fileUri: Uri, context: Context): Int {
         val uri = fileUri.path ?: ""
 
-        // An initial, naive validation
-        if (!(uri.contains("books_tracker", true) ||
-                    uri.contains("document", true) ||
-                    uri.contains("external", true))
-        ) {
-            (context as ListActivity).showSnackbar(uri.toString())
-            return false
-        }
-
-        // Read the first bytes of the file: every SQLite DB starts with the same string
-        val fileStream = context.contentResolver.openInputStream(fileUri)!!
-        try {
-            val fr = InputStreamReader(fileStream)
-            val buffer = CharArray(16)
-            fr.read(buffer, 0, 16)
-            val str = String(buffer)
-            fr.close()
-            if (str == "SQLite format 3\u0000")
-                return true
-        } catch (e: java.lang.Exception) {
-            (context as ListActivity).showSnackbar(e.toString())
-            e.printStackTrace()
-            return false
-        }
-        return false
+        return if (uri.contains("books_tracker_", true)) {
+            1
+        } else if (uri.contains("openreads_", true)) {
+            2
+        } else
+            0
     }
 }
