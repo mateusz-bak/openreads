c5626481d50f21f3fe6a7b29544ef1097264a6a7
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2022-06-02 23:18:06 +0100

    Books rated 0 starts won't be counted towards average ratings.

--- app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/StatisticsFragment.kt
+++ app/src/main/java/software/mdev/bookstracker/ui/bookslist/fragments/StatisticsFragment.kt
@@ -95,6 +95,7 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
                 var year: Int
 
                 var booksAllTime = 0
+                var booksAllTimeWithRating = 0
                 var pagesAllTime = 0
                 var sumRatingAllTime = 0F
 
@@ -122,6 +123,7 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
                 for (item_year in years) {
                     var booksInYear = 0
+                    var booksInYearWithRating = 0
                     var pagesInYear = 0
                     var sumRatingInYear = 0F
 
@@ -158,6 +160,10 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
                                 pagesAllTime += item_book.bookNumberOfPages
                                 sumRatingInYear += item_book.bookRating
                                 sumRatingAllTime += item_book.bookRating
+                                if (item_book.bookRating > 0F) {
+                                    booksInYearWithRating++
+                                    booksAllTimeWithRating++
+                                }
 
                                 // books by month
                                 when (Functions().convertLongToMonth(item_book.bookFinishDate.toLong())){
@@ -337,8 +343,8 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
                         avgPages = (pagesInYear/booksInYear)
 
                     var avgRating = 0F
-                    if (booksInYear != 0)
-                        avgRating = (sumRatingInYear/booksInYear)
+                    if (booksInYearWithRating != 0)
+                        avgRating = (sumRatingInYear/booksInYearWithRating)
 
                     listOfYears.add(Year(
                         item_year.toString(),
@@ -375,8 +381,8 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
                     avgPagesAllTime = (pagesAllTime/booksAllTime)
 
                 var avgRatingAllTime = 0F
-                if (booksAllTime != 0)
-                    avgRatingAllTime = (sumRatingAllTime/booksAllTime)
+                if (booksAllTimeWithRating != 0)
+                    avgRatingAllTime = (sumRatingAllTime/booksAllTimeWithRating)
 
                 listOfYears[0] = Year("0000",
                     booksAllTime,
