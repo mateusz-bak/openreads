740e6e95b6c05f907a5248d038af2b16e58e637d
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2022-10-16 14:59:20 +0100

    Deleted native Android code

--- app/src/main/java/software/mdev/bookstracker/data/db/entities/BookDetail.kt
+++
@@ -1,7 +0,0 @@
-package software.mdev.bookstracker.data.db.entities
-
-
-data class BookDetail(
-    var id: Int,
-    var value: String
-)
--- app/src/main/java/software/mdev/bookstracker/ui/bookslist/dialogs/ChallengeDialogListener.kt
+++
@@ -1,7 +0,0 @@
-package software.mdev.bookstracker.ui.bookslist.dialogs
-
-import software.mdev.bookstracker.data.db.entities.Year
-
-interface ChallengeDialogListener {
-    fun onSaveButtonClicked(item: Year)
-}
