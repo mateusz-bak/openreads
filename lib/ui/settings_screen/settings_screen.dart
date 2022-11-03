254989dad166f746290ea7ca9b899f2050ac78ca
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2022-11-03 16:28:29 +0000

    Added urls to settings

--- lib/ui/settings_screen/settings_screen.dart
+++ lib/ui/settings_screen/settings_screen.dart
@@ -7,6 +7,11 @@ import 'package:url_launcher/url_launcher.dart';
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
+  static const repoUrl = 'https://github.com/mateusz-bak/openreads-android';
+  static const translationUrl = 'https://crwd.in/openreads-android';
+  final releaseUrl = '$repoUrl/releases/tag/v2.0.0-alpha2';
+  final licenceUrl = '$repoUrl/blob/master/LICENSE';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,141 +40,166 @@ class SettingsScreen extends StatelessWidget {
                     }
                   },
                 ),
-                onPressed: (_) {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return Dialog(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
-                          backgroundColor:
-                              Theme.of(context).scaffoldBackgroundColor,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 20,
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Padding(
-                                  padding: EdgeInsets.only(left: 10),
-                                  child: Text(
-                                    'Select theme mode',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: SimpleDialogOption(
-                                        padding: EdgeInsets.zero,
-                                        onPressed: () {
-                                          BlocProvider.of<ThemeBloc>(context)
-                                              .add(const ChangeThemeEvent(
-                                                  ThemeMode.system));
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: const Padding(
-                                          padding: EdgeInsets.symmetric(
-                                            horizontal: 10,
-                                            vertical: 15,
-                                          ),
-                                          child: Text(
-                                            'Follow system mode',
-                                            style: TextStyle(fontSize: 16),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 5),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: SimpleDialogOption(
-                                        padding: EdgeInsets.zero,
-                                        onPressed: () {
-                                          BlocProvider.of<ThemeBloc>(context)
-                                              .add(const ChangeThemeEvent(
-                                                  ThemeMode.light));
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: const Padding(
-                                          padding: EdgeInsets.symmetric(
-                                            horizontal: 10,
-                                            vertical: 15,
-                                          ),
-                                          child: Text(
-                                            'Light mode',
-                                            style: TextStyle(fontSize: 16),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 5),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: SimpleDialogOption(
-                                        padding: EdgeInsets.zero,
-                                        onPressed: () {
-                                          BlocProvider.of<ThemeBloc>(context)
-                                              .add(const ChangeThemeEvent(
-                                                  ThemeMode.dark));
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: const Padding(
-                                          padding: EdgeInsets.symmetric(
-                                            horizontal: 10,
-                                            vertical: 15,
-                                          ),
-                                          child: Text(
-                                            'Dark mode',
-                                            style: TextStyle(fontSize: 16),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      });
-                },
+                onPressed: (context) => _showThemeModeDialog(context),
               ),
             ],
           ),
           SettingsSection(
             title: const Text('About'),
             tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                title: const Text('Version'),
-                description: const Text('2.0.0-alpha2'),
-                onPressed: (_) {
-                  launchUrl(
-                    Uri.parse(
-                      'https://github.com/mateusz-bak/openreads-android/releases/tag/v2.0.0-alpha2',
-                    ),
-                    mode: LaunchMode.externalNonBrowserApplication,
-                  );
-                },
+              _buildURLSetting(
+                title: 'Version',
+                description: '2.0.0-alpha2',
+                url: releaseUrl,
+              ),
+              _buildURLSetting(
+                title: 'Source code',
+                url: repoUrl,
+              ),
+              _buildURLSetting(
+                title: 'Changelog',
+                url: releaseUrl,
+              ),
+              _buildURLSetting(
+                title: 'Licence',
+                description: 'GNU General Public License v2.0',
+                url: licenceUrl,
+              ),
+              _buildURLSetting(
+                title: 'Help with translation',
+                url: translationUrl,
               ),
             ],
           ),
         ],
       ),
+    );
+  }
+
+  _showThemeModeDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 20,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      'Select theme mode',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: SimpleDialogOption(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            BlocProvider.of<ThemeBloc>(context)
+                                .add(const ChangeThemeEvent(ThemeMode.system));
+                            Navigator.of(context).pop();
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 15,
+                            ),
+                            child: Text(
+                              'Follow system mode',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: SimpleDialogOption(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            BlocProvider.of<ThemeBloc>(context)
+                                .add(const ChangeThemeEvent(ThemeMode.light));
+                            Navigator.of(context).pop();
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 15,
+                            ),
+                            child: Text(
+                              'Light mode',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: SimpleDialogOption(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            BlocProvider.of<ThemeBloc>(context)
+                                .add(const ChangeThemeEvent(ThemeMode.dark));
+                            Navigator.of(context).pop();
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 15,
+                            ),
+                            child: Text(
+                              'Dark mode',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  SettingsTile _buildURLSetting({
+    required String title,
+    String? description,
+    required String url,
+  }) {
+    return SettingsTile.navigation(
+      title: Text(title),
+      description: (description != null) ? Text(description) : null,
+      onPressed: (_) {
+        launchUrl(
+          Uri.parse(url),
+          mode: LaunchMode.externalNonBrowserApplication,
+        );
+      },
     );
   }
 }
