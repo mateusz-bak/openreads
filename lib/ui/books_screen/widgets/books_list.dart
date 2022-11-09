035bb70d2f689c5bbb7d4ebfc8927e985e2ae563
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2022-11-09 21:46:30 +0000

    Prserved list position

--- lib/ui/books_screen/widgets/books_list.dart
+++ lib/ui/books_screen/widgets/books_list.dart
@@ -3,7 +3,7 @@ import 'package:openreads/model/book.dart';
 import 'package:openreads/ui/book_screen/book_screen.dart';
 import 'package:openreads/ui/books_screen/widgets/widgets.dart';
 
-class BooksList extends StatelessWidget {
+class BooksList extends StatefulWidget {
   const BooksList({
     Key? key,
     required this.books,
@@ -12,22 +12,29 @@ class BooksList extends StatelessWidget {
   final List<Book> books;
 
   @override
+  State<BooksList> createState() => _BooksListState();
+}
+
+class _BooksListState extends State<BooksList>
+    with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return ListView.builder(
-      itemCount: books.length,
+      itemCount: widget.books.length,
       itemBuilder: (context, index) {
         return BookCard(
-          book: books[index],
+          book: widget.books[index],
           heroTag: "tag_$index",
-          addBottomPadding: (books.length == index + 1),
+          addBottomPadding: (widget.books.length == index + 1),
           onPressed: () {
-            if (books[index].id == null) return;
+            if (widget.books[index].id == null) return;
 
             Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (context) => BookScreen(
-                  id: books[index].id!,
+                  id: widget.books[index].id!,
                   heroTag: "tag_$index",
                 ),
               ),
@@ -37,4 +44,7 @@ class BooksList extends StatelessWidget {
       },
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
