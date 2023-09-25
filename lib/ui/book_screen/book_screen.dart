1c204a1457e3459afc50bc1cce6a5936dc66dd9e
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2023-09-25 23:36:13 +0100

    refactor: Extracted BookScreenAppBar widget.

--- lib/ui/book_screen/book_screen.dart
+++ lib/ui/book_screen/book_screen.dart
@@ -1,15 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:openreads/core/themes/app_theme.dart';
 import 'package:openreads/generated/locale_keys.g.dart';
-import 'package:openreads/logic/bloc/theme_bloc/theme_bloc.dart';
 import 'package:openreads/logic/cubit/current_book_cubit.dart';
-import 'package:openreads/logic/cubit/edit_book_cubit.dart';
 import 'package:openreads/main.dart';
 import 'package:openreads/model/book.dart';
-import 'package:openreads/ui/add_book_screen/add_book_screen.dart';
 import 'package:openreads/ui/book_screen/widgets/widgets.dart';
 
 class BookScreen extends StatelessWidget {
@@ -27,75 +23,6 @@ class BookScreen extends StatelessWidget {
 
     bookCubit.updateBook(book);
     context.read<CurrentBookCubit>().setBook(book);
-  }
-
-  _showDeleteRestoreDialog(
-    BuildContext context,
-    bool deleted,
-    bool? deletePermanently,
-    Book book,
-  ) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(cornerRadius),
-            ),
-            title: Text(
-              deleted
-                  ? deletePermanently == true
-                      ? LocaleKeys.delete_perm_question.tr()
-                      : LocaleKeys.delete_book_question.tr()
-                  : LocaleKeys.restore_book_question.tr(),
-              style: const TextStyle(fontSize: 18),
-            ),
-            actionsAlignment: MainAxisAlignment.spaceBetween,
-            actions: [
-              FilledButton.tonal(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(LocaleKeys.no.tr()),
-                ),
-              ),
-              FilledButton(
-                onPressed: () {
-                  if (deletePermanently == true) {
-                    _deleteBookPermanently(book);
-                  } else {
-                    _changeDeleteStatus(deleted, book);
-                  }
-
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pop();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(LocaleKeys.yes.tr()),
-                ),
-              ),
-            ],
-          );
-        });
-  }
-
-  Future<void> _changeDeleteStatus(bool deleted, Book book) async {
-    await bookCubit.updateBook(book.copyWith(
-      deleted: deleted,
-    ));
-
-    bookCubit.getDeletedBooks();
-  }
-
-  _deleteBookPermanently(Book book) async {
-    if (book.id != null) {
-      await bookCubit.deleteBook(book.id!);
-    }
-
-    bookCubit.getDeletedBooks();
   }
 
   IconData? _decideStatusIcon(int? status) {
@@ -232,99 +159,9 @@ class BookScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final moreButtonOptions = [
-      LocaleKeys.edit_book.tr(),
-    ];
-
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: PreferredSize(
-        preferredSize: AppBar().preferredSize,
-        // Needed to add BlocBuilder because the status bar was changing
-        // to different color then in BooksScreen
-        child: BlocBuilder<ThemeBloc, ThemeState>(
-          builder: (context, state) {
-            final themeMode = (state as SetThemeState).themeMode;
-
-            return AppBar(
-              backgroundColor: Colors.transparent,
-              scrolledUnderElevation: 0,
-              systemOverlayStyle: SystemUiOverlayStyle(
-                statusBarColor: Colors.transparent,
-                statusBarIconBrightness: themeMode == ThemeMode.system
-                    ? MediaQuery.platformBrightnessOf(context) ==
-                            Brightness.dark
-                        ? Brightness.light
-                        : Brightness.dark
-                    : themeMode == ThemeMode.dark
-                        ? Brightness.light
-                        : Brightness.dark,
-              ),
-              actions: [
-                BlocBuilder<CurrentBookCubit, Book>(
-                  builder: (context, state) {
-                    if (moreButtonOptions.length == 1) {
-                      if (state.deleted == true) {
-                        moreButtonOptions.add(LocaleKeys.restore_book.tr());
-                        moreButtonOptions.add(
-                          LocaleKeys.delete_permanently.tr(),
-                        );
-                      } else {
-                        moreButtonOptions.add(LocaleKeys.delete_book.tr());
-                      }
-                    }
-
-                    return PopupMenuButton<String>(
-                      onSelected: (_) {},
-                      itemBuilder: (_) {
-                        return moreButtonOptions.map((String choice) {
-                          return PopupMenuItem<String>(
-                            value: choice,
-                            child: Text(choice),
-                            onTap: () async {
-                              context.read<EditBookCubit>().setBook(state);
-
-                              await Future.delayed(const Duration(
-                                milliseconds: 0,
-                              ));
-                              if (!context.mounted) return;
-
-                              if (choice == moreButtonOptions[0]) {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (_) => const AddBookScreen(
-                                      editingExistingBook: true,
-                                    ),
-                                  ),
-                                );
-                              } else if (choice == moreButtonOptions[1]) {
-                                if (state.deleted == false) {
-                                  _showDeleteRestoreDialog(
-                                      context, true, null, state);
-                                } else {
-                                  _showDeleteRestoreDialog(
-                                      context, false, null, state);
-                                }
-                              } else if (choice == moreButtonOptions[2]) {
-                                _showDeleteRestoreDialog(
-                                  context,
-                                  true,
-                                  true,
-                                  state,
-                                );
-                              }
-                            },
-                          );
-                        }).toList();
-                      },
-                    );
-                  },
-                ),
-              ],
-            );
-          },
-        ),
-      ),
+      appBar: const BookScreenAppBar(),
       body: BlocBuilder<CurrentBookCubit, Book>(
         builder: (context, state) {
           return SingleChildScrollView(
