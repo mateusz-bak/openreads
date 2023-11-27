e20ed5d995d19926d1c6ebec0355f011964a42c7
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2023-11-27 22:56:08 +0000

    fix: Added missing CSV import button

--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -239,6 +239,7 @@ abstract class LocaleKeys {
   static const export_csv = 'export_csv';
   static const export_csv_description_1 = 'export_csv_description_1';
   static const import_goodreads_csv = 'import_goodreads_csv';
+  static const import_csv = 'import_csv';
   static const choose_not_finished_shelf = 'choose_not_finished_shelf';
   static const import_successful = 'import_successful';
   static const ok = 'ok';
