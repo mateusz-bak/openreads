ebac07b9ca3d3aef722ae4c8fa548e5e8cda1a75
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2023-12-06 22:52:58 +0000

    feat: Added example text when choosing font

--- lib/ui/settings_screen/widgets/settings_dialog_button.dart
+++ lib/ui/settings_screen/widgets/settings_dialog_button.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 
 class SettingsDialogButton extends StatelessWidget {
   const SettingsDialogButton({
-    Key? key,
+    super.key,
     required this.text,
     required this.onPressed,
-  }) : super(key: key);
+    this.fontFamily,
+  });
 
   final String text;
   final Function() onPressed;
+  final String? fontFamily;
+
+  static const example =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +28,28 @@ class SettingsDialogButton extends StatelessWidget {
                 horizontal: 10,
                 vertical: 10,
               ),
-              child: Text(
-                text,
-                style: const TextStyle(fontSize: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    text,
+                    style: TextStyle(fontSize: 16, fontFamily: fontFamily),
+                  ),
+                  fontFamily != null
+                      ? const Divider(
+                          height: 8,
+                        )
+                      : const SizedBox(),
+                  fontFamily != null
+                      ? Text(
+                          example,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: fontFamily,
+                          ),
+                        )
+                      : const SizedBox(),
+                ],
               ),
             ),
           ),
