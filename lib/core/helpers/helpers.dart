ba2b22ca05d9f11d0911ff220a3ea287aeb7a330
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2024-03-02 17:58:51 +0700

    fix: App will not try to find latest start or finish date for books that don't have any.

--- lib/core/helpers/helpers.dart
+++ lib/core/helpers/helpers.dart
@@ -25,6 +25,8 @@ DateTime? getLatestFinishDate(Book book) {
   final readingsWithFinishDate =
       book.readings.where((e) => e.finishDate != null);
 
+  if (readingsWithFinishDate.isEmpty) return null;
+
   return readingsWithFinishDate
       .map((e) => e.finishDate)
       .reduce((value, element) => value!.isAfter(element!) ? value : element);
@@ -34,6 +36,8 @@ DateTime? getLatestStartDate(Book book) {
   if (book.readings.isEmpty) return null;
 
   final readingsWithStartDate = book.readings.where((e) => e.startDate != null);
+
+  if (readingsWithStartDate.isEmpty) return null;
 
   return readingsWithStartDate
       .map((e) => e.startDate)
