e3db154b820f5923ebadf6e1b56764631420611b
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2024-04-01 22:22:37 +1100

    feat: Improved UI of cover placeholder

--- lib/ui/add_book_screen/widgets/cover_placeholder.dart
+++ lib/ui/add_book_screen/widgets/cover_placeholder.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:openreads/core/themes/app_theme.dart';
@@ -17,32 +18,45 @@ class CoverPlaceholder extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: InkWell(
-        customBorder: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(cornerRadius),
-        ),
-        onTap: onPressed,
-        child: Ink(
-          height: defaultHeight + 40,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(cornerRadius),
-            color: Theme.of(context).colorScheme.primary.withOpacity(0.3),
-            border: Border.all(color: dividerColor),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
-            child: Center(
-              child: Row(
-                children: [
-                  const SizedBox(width: 8),
-                  Icon(
-                    Icons.image,
-                    size: 24,
-                    color: Theme.of(context).colorScheme.primary,
+      child: DottedBorder(
+        borderType: BorderType.RRect,
+        radius: Radius.circular(cornerRadius),
+        dashPattern: const [10, 8],
+        color: Theme.of(context).colorScheme.onSurface.withOpacity(0.3),
+        strokeWidth: 2,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: InkWell(
+            customBorder: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(cornerRadius),
+            ),
+            onTap: onPressed,
+            child: Ink(
+              height: defaultHeight + 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(cornerRadius),
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
+                border: Border.all(color: dividerColor),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 0,
+                  vertical: 10,
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 8),
+                      Icon(
+                        Icons.image,
+                        size: 24,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      const SizedBox(width: 15),
+                      Text(LocaleKeys.click_to_add_cover.tr()),
+                    ],
                   ),
-                  const SizedBox(width: 15),
-                  Text(LocaleKeys.click_to_add_cover.tr()),
-                ],
+                ),
               ),
             ),
           ),
