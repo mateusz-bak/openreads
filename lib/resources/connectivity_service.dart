fc9ac8734f45d11dc0d32974e9fcc04b547197e2
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2024-04-07 20:41:46 +0700

    refactor: Fixed Dart format

--- lib/resources/connectivity_service.dart
+++ lib/resources/connectivity_service.dart
@@ -4,7 +4,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 class ConnectivityService {
   final _connectivity = Connectivity();
-  final connectivityStream = StreamController<List<ConnectivityResult>>.broadcast();
+  final connectivityStream =
+      StreamController<List<ConnectivityResult>>.broadcast();
 
   ConnectivityService() {
     _connectivity.onConnectivityChanged.listen((event) {
