37a301793937608db34965658a770b53c8b4f7e7
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2024-04-07 20:01:50 +0700

    build: Upgraded dependencies

--- lib/resources/connectivity_service.dart
+++ lib/resources/connectivity_service.dart
@@ -4,7 +4,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 class ConnectivityService {
   final _connectivity = Connectivity();
-  final connectivityStream = StreamController<ConnectivityResult>.broadcast();
+  final connectivityStream = StreamController<List<ConnectivityResult>>.broadcast();
 
   ConnectivityService() {
     _connectivity.onConnectivityChanged.listen((event) {
