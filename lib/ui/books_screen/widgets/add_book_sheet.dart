a96e314fda943e4cdf9dfeb7ef9365f134a45692
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2024-04-22 21:01:30 +0700

    feat: Improved add book sheet

--- lib/ui/books_screen/widgets/add_book_sheet.dart
+++ lib/ui/books_screen/widgets/add_book_sheet.dart
@@ -1,8 +1,9 @@
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:openreads/generated/locale_keys.g.dart';
-import 'package:openreads/ui/books_screen/widgets/widgets.dart';
 
 class AddBookSheet extends StatefulWidget {
   const AddBookSheet({
@@ -23,58 +24,54 @@ class AddBookSheet extends StatefulWidget {
 class _AddBookSheetState extends State<AddBookSheet> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const SizedBox(height: 10),
-        Container(
-          height: 3,
-          width: MediaQuery.of(context).size.width / 4,
-          decoration: BoxDecoration(
-            color: Colors.grey.shade300,
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(10, 20, 10, 40),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: IntrinsicHeight(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const SizedBox(width: 10),
-                Expanded(
-                  child: AddBookMethodButton(
-                    text: LocaleKeys.add_manually.tr(),
-                    icon: FontAwesomeIcons.solidKeyboard,
-                    onPressed: widget.addManually,
+    return Material(
+      child: SafeArea(
+        top: false,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (Platform.isAndroid)
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Container(
+                  height: 5,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .onSurfaceVariant
+                        .withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(50),
                   ),
                 ),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: AddBookMethodButton(
-                    text: LocaleKeys.add_search.tr(),
-                    icon: FontAwesomeIcons.magnifyingGlass,
-                    onPressed: widget.searchInOpenLibrary,
-                  ),
-                ),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: AddBookMethodButton(
-                    text: LocaleKeys.add_scan.tr(),
-                    icon: FontAwesomeIcons.barcode,
-                    onPressed: widget.scanBarcode,
-                  ),
-                ),
-                const SizedBox(width: 10),
-              ],
+              ),
+            ListTile(
+              title: Text(LocaleKeys.add_manually.tr()),
+              leading: FaIcon(
+                FontAwesomeIcons.solidKeyboard,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              onTap: widget.addManually,
             ),
-          ),
+            ListTile(
+              title: Text(LocaleKeys.add_search.tr()),
+              leading: FaIcon(
+                FontAwesomeIcons.magnifyingGlass,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              onTap: widget.searchInOpenLibrary,
+            ),
+            ListTile(
+              title: Text(LocaleKeys.add_scan.tr()),
+              leading: FaIcon(
+                FontAwesomeIcons.barcode,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              onTap: widget.scanBarcode,
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
