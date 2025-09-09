cde455b6ce4dd1014ebe7ebdce98e69bafdded46
mateusz.bak <32651935+mateusz-bak@users.noreply.github.com>
2025-09-09 17:06:38 +0200

    fix: Fixed visibility of Challenge Dialog

--- lib/ui/statistics_screen/widgets/challenge_dialog.dart
+++ lib/ui/statistics_screen/widgets/challenge_dialog.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:openreads/core/themes/app_theme.dart';
 import 'package:openreads/generated/locale_keys.g.dart';
+import 'package:openreads/ui/common/keyboard_dismissable.dart';
 
 class ChallengeDialog extends StatefulWidget {
   const ChallengeDialog({
@@ -30,7 +31,7 @@ class _ChallengeDialogState extends State<ChallengeDialog>
   static const double maxBooks = 50;
 
   static const double minPages = 0;
-  static const double maxPages = 15000;
+  static const double maxPages = 30000;
 
   double _booksSliderValue = 0;
   double _pagesSliderValue = 0;
@@ -147,50 +148,48 @@ class _ChallengeDialogState extends State<ChallengeDialog>
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(cornerRadius),
-      ),
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              '${LocaleKeys.set_books_goal_for_year.tr()} ${widget.year}:',
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Slider.adaptive(
-              value: _booksSliderValue,
-              min: minBooks,
-              max: maxBooks,
-              divisions: 50,
-              label: _booksSliderValue.round().toString(),
-              activeColor: Theme.of(context).colorScheme.primary,
-              onChanged: (double value) {
-                setState(() {
-                  _booksSliderValue = value;
-                  _booksController.text = _booksSliderValue.round().toString();
-                });
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return KeyboardDismissible(
+      child: Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(cornerRadius),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.surfaceDim,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
                 children: [
-                  Container(
-                    width: 150,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8,
-                      vertical: 0,
+                  Expanded(
+                    child: Text(
+                      '${LocaleKeys.set_books_goal_for_year.tr()} ${widget.year}:',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
+                  ),
+                ],
+              ),
+              Slider.adaptive(
+                value: _booksSliderValue,
+                min: minBooks,
+                max: maxBooks,
+                divisions: 50,
+                label: _booksSliderValue.round().toString(),
+                activeColor: Theme.of(context).colorScheme.primary,
+                onChanged: (double value) {
+                  setState(() {
+                    _booksSliderValue = value;
+                    _booksController.text =
+                        _booksSliderValue.round().toString();
+                  });
+                },
+              ),
+              Row(
+                children: [
+                  Expanded(
                     child: Platform.isIOS
                         ? CupertinoTextField(
                             textAlign: TextAlign.center,
@@ -216,76 +215,75 @@ class _ChallengeDialogState extends State<ChallengeDialog>
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 30),
-            Row(
-              children: [
-                Switch.adaptive(
-                  value: _showPagesChallenge,
-                  activeColor: Theme.of(context).colorScheme.primary,
-                  onChanged: (value) {
-                    setState(() {
-                      _showPagesChallenge = value;
-                    });
-
-                    if (value) {
-                      _animController.forward();
-                    } else {
-                      _animController.animateBack(0,
-                          duration: const Duration(
-                            milliseconds: 250,
-                          ));
-                    }
-                  },
-                ),
-                const SizedBox(width: 10),
-                Text(
-                  LocaleKeys.add_pages_goal.tr(),
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 30),
-            SizeTransition(
-              sizeFactor: _animation,
-              child: Column(
+              const SizedBox(height: 30),
+              Row(
                 children: [
-                  Text(
-                    LocaleKeys.set_pages_goal.tr(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Slider.adaptive(
-                    value: _pagesSliderValue,
-                    min: minPages,
-                    max: maxPages,
-                    divisions: 150,
-                    label: _pagesSliderValue.round().toString(),
+                  Switch.adaptive(
+                    value: _showPagesChallenge,
                     activeColor: Theme.of(context).colorScheme.primary,
-                    onChanged: (double value) {
+                    onChanged: (value) {
                       setState(() {
-                        _pagesSliderValue = value;
-                        _pagesController.text =
-                            _pagesSliderValue.round().toString();
+                        _showPagesChallenge = value;
                       });
+
+                      if (value) {
+                        _animController.forward();
+                      } else {
+                        _animController.animateBack(0,
+                            duration: const Duration(
+                              milliseconds: 250,
+                            ));
+                      }
                     },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Text(
+                      LocaleKeys.add_pages_goal.tr(),
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              SizeTransition(
+                sizeFactor: _animation,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            LocaleKeys.set_pages_goal.tr(),
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Slider.adaptive(
+                      value: _pagesSliderValue,
+                      min: minPages,
+                      max: maxPages,
+                      divisions: 60,
+                      label: _pagesSliderValue.round().toString(),
+                      activeColor: Theme.of(context).colorScheme.primary,
+                      onChanged: (double value) {
+                        setState(() {
+                          _pagesSliderValue = value;
+                          _pagesController.text =
+                              _pagesSliderValue.round().toString();
+                        });
+                      },
+                    ),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Container(
-                          width: 150,
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 8,
-                            vertical: 0,
-                          ),
+                        Expanded(
                           child: Platform.isIOS
                               ? CupertinoTextField(
                                   textAlign: TextAlign.center,
@@ -315,46 +313,46 @@ class _ChallengeDialogState extends State<ChallengeDialog>
                         ),
                       ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Platform.isIOS
-                ? CupertinoButton(
-                    child: const Text("Save"),
-                    onPressed: () {
-                      widget.setChallenge(
-                          _booksTarget ?? 0,
-                          _showPagesChallenge ? _pagesTarget ?? 0 : 0,
-                          widget.year);
+              const SizedBox(height: 10),
+              Platform.isIOS
+                  ? CupertinoButton(
+                      child: const Text("Save"),
+                      onPressed: () {
+                        widget.setChallenge(
+                            _booksTarget ?? 0,
+                            _showPagesChallenge ? _pagesTarget ?? 0 : 0,
+                            widget.year);
 
-                      if (mounted) {
-                        Navigator.of(context).pop();
-                      }
-                    },
-                  )
-                : FilledButton(
-                    onPressed: () {
-                      widget.setChallenge(
-                          _booksTarget ?? 0,
-                          _showPagesChallenge ? _pagesTarget ?? 0 : 0,
-                          widget.year);
+                        if (mounted) {
+                          Navigator.of(context).pop();
+                        }
+                      },
+                    )
+                  : FilledButton(
+                      onPressed: () {
+                        widget.setChallenge(
+                            _booksTarget ?? 0,
+                            _showPagesChallenge ? _pagesTarget ?? 0 : 0,
+                            widget.year);
 
-                      if (mounted) {
-                        Navigator.of(context).pop();
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(cornerRadius),
+                        if (mounted) {
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(cornerRadius),
+                        ),
                       ),
-                    ),
-                    child: const Center(
-                      child: Text("Save"),
-                    ),
-                  )
-          ],
+                      child: const Center(
+                        child: Text("Save"),
+                      ),
+                    )
+            ],
+          ),
         ),
       ),
     );
