9b115f5d2fe8248f69baba2a4fde82a369696272
Rafael <rafael@rafael.me.uk>
2025-12-09 19:31:48 +0000

    Add publisher to book card list

--- lib/ui/books_screen/widgets/book_card_list_compact.dart
+++ lib/ui/books_screen/widgets/book_card_list_compact.dart
@@ -100,9 +100,9 @@ class BookCardListCompact extends StatelessWidget {
               color: Theme.of(context).colorScheme.onSurface.withOpacity(0.8),
             ),
           ),
-          book.publicationYear != null
+          (book.publisher != null || book.publicationYear != null)
               ? Text(
-                  book.publicationYear.toString(),
+                  '${book.publisher ?? ''}${book.publisher != null && book.publicationYear != null ? ', ' : ''}${book.publicationYear ?? ''}',
                   softWrap: true,
                   overflow: TextOverflow.clip,
                   style: TextStyle(
